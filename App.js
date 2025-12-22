5cbee30ba92c37c204741275462a5c4425abefb2
Franciszek <FCzajkowski@proton.me>
2025-12-22 23:49:51 +0100

    Refactor Home component and update buttons

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 // ##############################################
-// THIS IS BASIC EXAMPLE OF BUDDYJS USAGE
-// FOR MORE COMPLEX EXAMPLES CHECK THE GITHUB REPO
+// THIS IS BASIC EXAMPLE OF BUDDY.JS USAGE
 // ##############################################
 
-
 import { mount } from './src/render.js';
-import importStyle from './src/importStyle.js';
 import { template } from './src/template.js';
+import importStyle from './src/importStyle.js';
 importStyle('style.css');
 
-let name = 'BuddyJS User';
-
 function Home() {
-  return template`
-    <div class="buddyjs-home">
-      <h1>Hello ${name}</h1>
-    </div>
-  `;
+  return template(`
+    <main>
+      <h1>
+        Buddy<span class="js-yellow">JS</span>
+      </h1>
+      <p>thank you for choosing BuddyJS :)</p>
+      <div class="button-row">
+        <button class="main-btn" id="github-btn">Github</button>
+        <button class="main-btn" id="socials-btn">Socials</button>
+      </div>
+    </main>
+  `);
 }
 
 const root = document.getElementById('app');
 mount(Home, root);
+
+const openLink = (url) => {
+  window.open(url, '_blank', 'noopener');
+}
+document.addEventListener('click', (e) => {
+  if (e.target.id === 'github-btn') openLink('https://github.com/FCzajkowski/BuddyJS');
+  if (e.target.id === 'socials-btn') openLink('https://x.com/f_czajkowski');
+});
+
